ed0374cbb36c056a6cf5edd966d843b2f8a5eccc
Amine-youcode <bourhymamine@gmail.com>
2024-09-30 10:45:11 +0200

    fonction2

---
+++ fonction2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int somme(int a,int b){
+    
+     
+    return a*b;
+}
+
+int main() {
+ int a,b,result;
+printf("entrer une number: ");
+scanf("%d",&a);
+printf("entrer une number: ");
+scanf("%d",&b);
+ result=somme(a,b);
+printf("la somme est %d",result);
+
+    return 0;
+}
